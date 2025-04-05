ca4cdf8162188f8d008f0c06684503ef194de479
Shinseojin <02sjin@gmail.com>
2025-04-05 18:16:25 +0900

    10926번 문자열 ??! 출력 문제 풀이

---
+++ src/main/java/bronze/B10926.java
@@ -0,0 +1,12 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class B10926 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String a =sc.next();
+
+        System.out.print(a+"??!");
+    }
+}
