c1548a97e25b8b9676c8f3c236fec5a3525308b4
Shinseojin <02sjin@gmail.com>
2025-04-06 16:53:46 +0900

    2588번 곱셈 문제 풀이

---
+++ src/main/java/step1/B2588.java
@@ -0,0 +1,20 @@
+package step1;
+
+import java.util.Scanner;
+
+public class B2588 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        int A = Integer.parseInt(a);
+
+        for(int i = b.length() -1; i >= 0; i--){
+            char ch = b.charAt(i);
+            int c = ch - '0';
+            System.out.println(A * c);
+        }
+    System.out.println(A * Integer.parseInt(b));
+    }
+}
