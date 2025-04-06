9567a7b6d1a827cc7666008e53a01ca3ce7a5340
Shinseojin <02sjin@gmail.com>
2025-04-06 15:16:18 +0900

    18108번 연도변환 출력 문제 풀이

---
+++ src/main/java/bronze/B18108.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class B18108 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.print(a-543);
+    }
+}
+
