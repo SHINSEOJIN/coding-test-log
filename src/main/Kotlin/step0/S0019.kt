f69b775718902d1a48e66fb7acf6d773d80562e8
Shinseojin <02sjin@naver.com>
2025-04-27 19:24:20 +0900

    S0019 입력값까지 소수 판별 및 출력

---
+++ src/main/Kotlin/step0/S0019.kt
@@ -0,0 +1,23 @@
+package step0
+
+fun main() {
+    try {
+        println("숫자를 입력하세요.")
+        var num = readLine()!!.toInt()
+        for (i in 2..num) {
+            var isPrime = true
+            for (l in 2 until i) {
+                if (i % l == 0) {
+                    isPrime = false
+                    break
+                }
+            }
+            if (isPrime) {
+                println(i)
+            }
+        }
+    } catch (e: Exception) {
+        println("잘못된 입력입니다.")
+    }
+}
+
