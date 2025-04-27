1f98727bf1aa31fc1c3aa2cb6f76cac26f34d11c
Shinseojin <02sjin@naver.com>
2025-04-27 18:40:30 +0900

    S0016 1부터 입력값까지 소수 출력(예외처리 추가)

--- src/main/Kotlin/step0/S0016.kt
+++ src/main/Kotlin/step0/S0016.kt
@@ -4,18 +4,22 @@ package step0
 //소수만(나머지가 1과 본인뿐인 숫자) 출력
 
 fun main() {
-    println("숫자를 입력하세요.")
-    var number = readLine()!!.toInt()
-    for (n in 1..number) {
-        var isPrime = true
-        for (m in 2..n - 1) {
-            if (n % m == 0) {
-                isPrime = false
-                break
+    try {
+        println("숫자를 입력하세요.")
+        var number = readLine()!!.toInt()
+        for (n in 2..number) {
+            var isPrime = true
+            for (m in 2..n - 1) {
+                if (n % m == 0) {
+                    isPrime = false
+                    break
+                }
+            }
+            if (isPrime) {
+                println(n)
             }
         }
-        if (isPrime) {
-            println(n)
-        }
+    } catch (e: Exception) {
+        println("잘못된 입력입니다.")
     }
 }
