a3db50c4d87d4ef17d3e3ac8a0b3359547573d6b
Shinseojin <02sjin@naver.com>
2025-04-27 18:36:34 +0900

    S0016 1부터 입력값까지 소수 출력

---
+++ src/main/Kotlin/step0/S0016.kt
@@ -0,0 +1,21 @@
+package step0
+
+//사용자에게 숫자를 입력받아
+//소수만(나머지가 1과 본인뿐인 숫자) 출력
+
+fun main() {
+    println("숫자를 입력하세요.")
+    var number = readLine()!!.toInt()
+    for (n in 1..number) {
+        var isPrime = true
+        for (m in 2..n - 1) {
+            if (n % m == 0) {
+                isPrime = false
+                break
+            }
+        }
+        if (isPrime) {
+            println(n)
+        }
+    }
+}
