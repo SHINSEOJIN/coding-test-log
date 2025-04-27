559747161842f83578aa654a9d15c505686c7327
Shinseojin <02sjin@naver.com>
2025-04-27 19:29:28 +0900

    S0020 1~100 사이 3,5,7 공배수 없을 경우 메시지 출력

---
+++ src/main/Kotlin/step0/S0020.kt
@@ -0,0 +1,12 @@
+package step0
+
+fun main(){
+    for(i in 1..100){
+        if(i % 3 == 0 && i % 5 == 0 && i % 7 == 0){
+            println(i)
+        }else{
+            println("조건에 맞는 숫자가 없습니다.")
+            break
+        }
+    }
+}
