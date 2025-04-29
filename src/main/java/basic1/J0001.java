82153392db7c9b4df62c84205b95f4a773eb7eeb
Shinseojin <02sjin@naver.com>
2025-04-29 22:44:32 +0900

    복습 - a+b 입력 후 출력

---
+++ src/main/java/basic1/J0001.java
@@ -0,0 +1,15 @@
+package basic1;
+
+import java.util.Scanner;
+
+public class J0001 {
+
+    public static void main(String[]args){
+        Scanner sc = new Scanner();
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(a+b);
+    }
+}
