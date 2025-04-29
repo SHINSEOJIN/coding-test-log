63f590d0c67c267753a0be998be2f4aa5e9cd858
Shinseojin <02sjin@naver.com>
2025-04-29 10:42:37 +0900

    feat: 사용자 입력과 로또 번호 비교 기능 구현 (2단계 완료)

--- src/main/Kotlin/step0_1/S0102.kt
+++ src/main/Kotlin/step0_1/S0102.kt
@@ -3,10 +3,15 @@ package step0_1
 import kotlin.random.Random
 
 fun main() {
-    lottomachine()
+
+    val winningNumbers = lottoMachine()    // 당첨 번호 리스트
+    val userNumbers = inputLotto()         // 사용자 입력 번호 리스트
+
+    compareLotto(winningNumbers, userNumbers) //로또 번호 비교
 }
 
-fun lottomachine() {
+//로또번호 자동생성
+fun lottoMachine() : Set<Int> {
     val numbers = mutableListOf<Int>()
 
     while (numbers.size < 6) {
@@ -15,5 +20,35 @@ fun lottomachine() {
             numbers.add(number)
         }
     }
-    println(numbers.sorted())
+    println("테스트프린트" + numbers.sorted())
+    return numbers.toSet()
+}
+
+
+//로또번호 입력받기
+fun inputLotto() : Set<Int> {
+
+    while (true) {
+        println("숫자 6개를 입력하세요. (1 ~ 45사이)")
+        val input = readLine() ?: continue
+        val numbers = input.trim()
+            .split(Regex("\\s+"))
+            .mapNotNull{it.toIntOrNull()}
+            .filter{it in 1..45}
+            .toSet()
+
+        if(numbers.size == 6){
+            return numbers
+        }else{
+            println("1부터 45까지의 숫자를 정확히 입력해주세요.")
+        }
+    }
+}
+
+//로또번호 비교하고 개수 출력
+fun compareLotto(winnig: Set<Int>, user: Set<Int>){
+    val match = winnig.intersect(user).size
+    println("당첨번호: ${winnig.sorted()}")
+    println("입력번호: ${user.sorted()}")
+    println("맞은 개수: $match 개")
 }
