fec3658baeece6ba0ed53909f384525d3423cf09
Shinseojin <02sjin@naver.com>
2025-04-29 22:51:11 +0900

    복습 - a+b 입력 후 출력 (오기수정)

--- src/main/java/basic1/J0001.java
+++ src/main/java/basic1/J0001.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class J0001 {
 
     public static void main(String[]args){
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int b = sc.nextInt();
