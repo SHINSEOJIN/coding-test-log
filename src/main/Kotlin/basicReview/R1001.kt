f75598c323dd764c074185764e3f3e7e396d1d8f
Shinseojin <02sjin@naver.com>
2025-04-29 22:21:55 +0900

    R0001 복습 - 문자열 뒤집기(문자열검증추가)

---
+++ src/main/Kotlin/basicReview/R1001.kt
@@ -0,0 +1,12 @@
+package basicReview
+
+fun main() {
+    val input = readLine()!!
+    val isAllLetter = input.all { it.isLetter() }
+
+    if (isAllLetter) {
+        println(input.reversed())
+    }else{
+        println("잘못된 입력입니다.")
+    }
+}
