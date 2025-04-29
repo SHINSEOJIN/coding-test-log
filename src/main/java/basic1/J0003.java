cb391ad8ecc1bd480064a8fd1fd3bef37a62fe83
Shinseojin <02sjin@naver.com>
2025-04-29 23:18:29 +0900

    review: J0003 a+b 출력 복습 (숫자 여부 검증 추가/ 추후 검증 강화 예정)

--- src/main/java/basic1/J0003.java
+++ src/main/java/basic1/J0003.java
@@ -3,15 +3,15 @@ package basic1;
 import java.util.Scanner;
 
 public class J0003 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         System.out.println("더할 숫자 2개를 입력해주세요.");
 
         Scanner sc = new Scanner(System.in);
-        if(sc.hasNext()) {
+        if (sc.hasNext()) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             System.out.println("총계: " + (a + b));
-        }else{
+        } else {
             System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
         }
 
