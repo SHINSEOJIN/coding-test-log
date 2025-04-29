3d6792bba4e694f06cbec7aba0ae7cc847084dc8
Shinseojin <02sjin@naver.com>
2025-04-29 21:06:48 +0900

    S0032 팰린드롬 단어 판별/ 문자 여부 검증 강화

--- src/main/Kotlin/step0/S0032.kt
+++ src/main/Kotlin/step0/S0032.kt
@@ -2,13 +2,13 @@ package step0
 
 //사용자의 입력을 받아 팰린드롬"(앞에서 읽어도 뒤에서 읽어도 같은 단어)인지 판별하는 프로그램
 
-fun main (){
+fun main() {
     val input = readLine()!!.split(Regex("\\s+"))
-
-    if(input == input.reversed()){
-        println("팰린드롬입니다.")
-    }else if(input != input.reversed()){
-        println("팰린드롬이 아닙니다.")
+    if (input.all { word -> word.all { it.isLetter() } }) {
+        val combined = input.joinToString("").lowercase()
+        if (combined == combined.reversed()) {
+            println("팰린드롬입니다.")
+        } else
+            println("팰린드롬이 아닙니다.")
     }
-
 }
