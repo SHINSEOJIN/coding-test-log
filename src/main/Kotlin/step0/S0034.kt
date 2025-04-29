e0b41f97b4343eb65ca42cb83de3738beddeb3ff
Shinseojin <02sjin@naver.com>
2025-04-29 21:36:04 +0900

    S0034 공백/대소문자 무시한 팰린드롬 검사 기능 추가

---
+++ src/main/Kotlin/step0/S0034.kt
@@ -0,0 +1,18 @@
+package step0
+
+fun main() {
+    val input = readLine()!!
+    val cleaned = input.replace("\\s+".toRegex(),"").lowercase()
+    val isAllLetter = cleaned.all { it.isLetter() }
+
+    if(isAllLetter){
+        if(cleaned == cleaned.reversed()){
+            println("팰린드롬입니다.")
+        }else{
+            println("팰린드롬이 아닙니다.")
+        }
+    }else{
+        println("잘못된 입력입니다.")
+    }
+
+}
