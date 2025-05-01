bd43bf08b55aa21916a9ed9720a79c8cbe7173a9
Shinseojin <02sjin@naver.com>
2025-05-01 10:45:06 +0900

    Feat: 입력한 숫자 5개 중 가장 큰 수 출력(개수검증강화)

---
+++ src/main/Kotlin/basic2/S0036.kt
@@ -0,0 +1,15 @@
+package basic2
+
+fun main(){
+    println("5개의 숫자를 입력해주세요")
+    val input = readLine()!!
+        .split(Regex("[\\s]+"))
+        .mapNotNull { it.toIntOrNull() }
+
+    if(input.size == 5){
+        val max = input.maxOrNull()
+        println("가장 큰 숫자는 $max 입니다.")
+    }else{
+        println("5개의 숫자를 입력해주세요.")
+    }
+}
