d394512ff0d655bc98bc05fa03697fa4fb641c20
Shinseojin <02sjin@naver.com>
2025-05-02 13:42:56 +0900

    Feat: 배열에서 짝수의 합과 홀수 개수 구하는 조건문

--- src/main/Kotlin/basic2/S0043.kt
+++ src/main/Kotlin/basic2/S0043.kt
@@ -3,14 +3,14 @@ package basic2
 //정수 배열이 주어졌을 때,
 //**짝수의 합**과 **홀수의 개수**를 각각 구해서 출력하시오.
 
-fun main(){
+fun main() {
     val numbers = arrayOf(3, 8, 5, 12, 9, 10, 1)
     var sum = 0
     var cnt = 0
-    for(num in numbers){
-        if(num % 2 == 0){
+    for (num in numbers) {
+        if (num % 2 == 0) {
             sum += num
-        }else if(num % 2 == 1){
+        } else if (num % 2 == 1) {
             cnt++
         }
     }
